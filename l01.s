c2b2c9fca89d75a9b4c18ee1e137542963fb6794
aidantrabs <aidantraboulay13@gmail.com>
2022-01-06 18:34:42 -0400

    Lab 01

---
+++ l01.s
@@ -0,0 +1,26 @@
+/*
+-------------------------------------------------------
+l01.s
+Assign to and add contents of registers.
+-------------------------------------------------------
+Author:  Aidan Traboulay
+ID:      200115590
+Email:   trab5590@wlu.ca
+Date:    2021-01-06
+-------------------------------------------------------
+*/
+
+.org 0x1000 // set origin to 0x1000
+.text // declare text segment
+.global _start // declare entry point
+_start:
+
+MOV r0, #9 // Store decimal 9 in register r0
+MOV r1, #0xE // Store hexadecimal 0xE (decimal 14) in register r1
+ADD r2, r1, r0 // Add contents of r1 and r0 and store result in r2
+
+// End program
+_stop:
+B _stop // Branch to stop
+
+.end // end program
