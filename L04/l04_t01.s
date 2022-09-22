a49e2532acb988504410c3b31cceba9c7060b572
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-09-22 02:57:42 -0400

    Update Lab 4

--- L04/l04_t01.s
+++ L04/l04_t01.s
@@ -2,7 +2,7 @@
 -------------------------------------------------------
 l04.s
 Task 1
-{description}
+Reads a string from the UART
 -------------------------------------------------------
 Author:  Aidan Traboulay
 ID:      200115590
@@ -10,3 +10,31 @@ Email:   trab5590@wlu.ca
 Date:    
 -------------------------------------------------------
 */
+
+.equ UART_BASE, 0xff201000     
+.equ SIZE, 80                 
+.equ VALID, 0x8000             
+.equ ENTER, 0x0a
+.org    0x1000                 
+.text  
+.global _start
+
+// Constants
+_start:
+     LDR            R1, =UART_BASE
+          
+     LOOP:
+          LDRB      R0, [R1]    
+          CMP       R0, #0x0a   
+          BEQ       _stop       
+          STRB      R0, [R1]    
+          B         LOOP
+
+_stop:
+     B    _stop
+
+.data  
+
+READ_STRING:  .space   SIZE
+
+.end
--- L04/l04_t02.s
+++ L04/l04_t02.s
@@ -2,7 +2,7 @@
 -------------------------------------------------------
 l04.s
 Task 2
-{description}
+Reads a string from the UART
 -------------------------------------------------------
 Author:  Aidan Traboulay
 ID:      200115590
@@ -10,3 +10,40 @@ Email:   trab5590@wlu.ca
 Date:    
 -------------------------------------------------------
 */
+
+.equ UART_BASE, 0xff201000     
+.equ SIZE, 80                  
+.equ VALID, 0x8000             
+.equ ENTER, 0x0a
+.org    0x1000                 
+.text  
+.global _start
+
+// Constants
+_start:
+
+     LDR     R1, =UART_BASE
+     LDR     R4, =READ_STRING
+     LDR     R7, =ENTER
+     ADD     R5, R4, #SIZE         
+          
+     LOOP:
+     LDRB    R0, [R1] 		           
+     CMP     R0, R7			           
+     BEQ     _stop			             
+     STRB    R0, [R4]               
+     ADD     R4, R4, #1             
+     CMP     R4, R5                 
+     BEQ     _stop
+     B       LOOP
+
+// Read string from UART
+_stop:
+     B    _stop 
+
+.data // Data section
+
+// Global variables
+READ_STRING:  .space    SIZE
+
+.end
